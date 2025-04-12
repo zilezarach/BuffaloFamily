aaed9b393abe6bfcda23e7c133cbe233235afff4
zilezarach <zarachiistephen@gmail.com>
2025-04-13 00:04:37 +0300

    added products

--- app/assets/data.js
+++ app/assets/data.js
@@ -12,98 +12,167 @@ import img11 from "./Tshirt4.jpeg";
 import img12 from "./Tshirt5.jpeg";
 import img13 from "./Tshirt6.jpeg";
 import img14 from "./Tshirt7.jpeg";
-
+import img15 from "./hoodiesBlue.jpeg";
+import img16 from "./hoodiesGrey.jpeg";
+import img17 from "./hoodiesBlack.jpeg";
+import img18 from "./hatRed.jpeg";
+import img19 from "./hatBlue.jpeg";
+import img20 from "./hatBlack.jpeg";
+import img21 from "./marvin blue.jpeg";
+import img22 from "./marvin black.jpeg";
+import img23 from "./marvin white.jpeg";
+import img24 from "./Bag.jpeg";
+import { image } from "framer-motion/client";
 let product = [
   {
     id: 1,
     name: "Buffa T-shirt (Blue)",
     price: 1400,
-    image: img1
+    image: img1,
   },
 
   {
     id: 2,
     name: "Buffa T-shirt (Pink)",
     price: 1400,
-    image: img3
+    image: img3,
   },
 
   {
     id: 3,
     name: "Buffa Shirt (Black)",
     price: 1400,
-    image: img2
+    image: img2,
   },
 
   {
     id: 4,
     name: "Buffa Shirt (White)",
     price: 1400,
-    image: img4
+    image: img4,
   },
 
   {
     id: 5,
     name: "Buffa notebook (Black)",
     price: 2000,
-    image: img5
+    image: img5,
   },
 
   {
     id: 6,
     name: "Buffa notebook (Grey)",
     price: 2000,
-    image: img6
+    image: img6,
   },
   {
     id: 7,
     name: "Buffa Hoodie",
     price: 3000,
-    image: img7
+    image: img7,
   },
   {
     id: 8,
     name: " APD Anti Poverty Department (Pink)",
     price: 1200,
-    image: img8
+    image: img8,
   },
   {
     id: 9,
     name: " APD Anti Poverty Department (Yellow)",
     price: 1200,
-    image: img9
+    image: img9,
   },
   {
     id: 10,
     name: "APD Anti Poverty Department (Orange)",
     price: 1200,
-    image: img10
+    image: img10,
   },
   {
     id: 11,
     name: " APD Anti Poverty Department (Black)",
     price: 1200,
-    image: img11
+    image: img11,
   },
   {
     id: 12,
     name: " APD Anti Poverty Department (Blue)",
     price: 1200,
-    image: img12
+    image: img12,
   },
   {
     id: 13,
     name: "APD Anti Poverty Department(Grey)",
     price: 1200,
-    image: img13
+    image: img13,
   },
   {
     id: 14,
     name: " APD Anti Poverty Department (White)",
     price: 1200,
-    image: img14
-  }
+    image: img14,
+  },
+  {
+    id: 15,
+    name: "Buffalo hoodie (Blue)",
+    price: 3000,
+    image: img15,
+  },
+  {
+    id: 16,
+    name: "Buffalo hoodies (Grey)",
+    price: 3000,
+    image: img16,
+  },
+  {
+    id: 17,
+    name: "Buffalo hoodie (Black)",
+    price: 3000,
+    image: img17,
+  },
+  {
+    id: 18,
+    name: "Buffalo hat (Red)",
+    price: 500,
+    image: img18,
+  },
+  {
+    id: 19,
+    name: "Buffalo hat (Blue)",
+    price: 500,
+    image: img19,
+  },
+  {
+    id: 20,
+    name: "Buffalo hat (Black)",
+    price: 500,
+    image: img20,
+  },
+  {
+    id: 21,
+    name: "Marvin Blue",
+    price: 600,
+    image: img21,
+  },
+  {
+    id: 22,
+    name: "Marvin Black",
+    price: 600,
+    image: img22,
+  },
+  {
+    id: 23,
+    name: "Marvin White",
+    price: 600,
+    image: img23,
+  },
+  {
+    id: 24,
+    name: "Tote Bag",
+    price: 500,
+    image: img24,
+  },
 ];
 
 export default product;
-
